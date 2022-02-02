93593d18976b51016122f3564ddf2cb8d0112e11
Piero A. Pretto <pretto417@gmail.com>
2022-02-02 10:05:35 +0300

    add documentation to 03 challenge; add test to 04 spec file

--- coding_challenges/problems/03_letter_count.js
+++ coding_challenges/problems/03_letter_count.js
@@ -2,6 +2,10 @@
 // returns an object mapping each letter to its frequency. Do not include
 // spaces.
 
+// For example: 
+// letterCount("moon") should return { m: 1, o: 2, n: 1 };
+
+
 function letterCount(str) {
 
 }
